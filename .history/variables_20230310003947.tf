8e9516547c51dcda47b710fba16ee92e338c0f6a
Jeraldinne Molleda <jeraldinnemg@gmail.com>
2023-03-10 00:59:33 -0300

    terraform files

---
+++ .history/variables_20230310003947.tf
@@ -0,0 +1,24 @@
+# ==================================================================q
+# Proposito:    declaramos todas las variables que vamos a usar
+# Autor:        DH
+# Fecha:        08.03.23
+# Version:      1.0
+# ==================================================================
+
+variable "main_vpc_cidr" {
+   description = "Nuestro Security Group"  
+   default = "10.0.0.0/24"
+}
+
+variable "public_subnets" {
+   description = "subnet con acceso a internet"
+   default = "10.0.0.128/26"
+}
+
+
+variable "private_subnets" {
+   description = "subnet sin acceso a internet"
+   default = "10.0.0.192/26"
+}
+
+# ==================================================================
